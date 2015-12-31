15d5655a01b71664a69726799c700d3a29c4d1fa
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2015-12-31 10:36:32 +0700

    add generic form type

---
+++ Toolkit/Form/GenericFromType.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Symfonian\Indonesia\AdminBundle\Form;
+
+/*
+ * Author: Muhammad Surya Ihsanuddin<surya.kejawen@gmail.com>
+ * Url: https://github.com/ihsanudin
+ */
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GenericFormType extends AbstractType
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    protected $entity;
+
+    protected $translationDomain;
+
+    public function __construct(ContainerInterface $container, $translationDomain = null)
+    {
+        $this->container = $container;
+        $this->translationDomain = $translationDomain;
+    }
+
+    public function setEntity($entity)
+    {
+        if (is_object($entity)) {
+            $entity = get_class($entity);
+        }
+
+        $this->entity = $entity;
+    }
+
+    public function setTranslationDomain($translationDomain)
+    {
+        $this->translationDomain = $translationDomain;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        foreach ($options['fields'] as $key => $value) {
+            if ('id' === $value) {
+                continue;
+            }
+
+            $builder->add($value, null, array(
+                'attr' => array(
+                    'class' => $options['attr']['class'],
+                ),
+            ));
+        }
+
+        $builder->add('save', SubmitType::class, array(
+            'label' => 'action.submit',
+            'attr' => array(
+                'class' => 'btn btn-primary',
+            ),
+        ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => $this->entity,
+            'translation_domain' => $this->translationDomain,
+            'intention' => $this->getName(),
+        ));
+
+        $resolver->setRequired(array('fields'));
+    }
+
+    public function getName()
+    {
+        return 'generic';
+    }
+}
