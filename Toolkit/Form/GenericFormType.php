e8ba84b30e76a85b666823b8f1dcc18bae524d90
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2015-12-31 11:30:41 +0700

    typo service name

--- Toolkit/Form/GenericFormType.php
+++ Toolkit/Form/GenericFormType.php
@@ -13,7 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class GenericFromType extends AbstractType
+class GenericFormType extends AbstractType
 {
     /**
      * @var ContainerInterface
