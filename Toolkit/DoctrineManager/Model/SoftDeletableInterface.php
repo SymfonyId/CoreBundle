305626d93ef5d53891f145c27959e6aaed3265a3
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-01-04 13:06:42 +0700

    improve php doc

--- Toolkit/DoctrineManager/Model/SoftDeletableInterface.php
+++ Toolkit/DoctrineManager/Model/SoftDeletableInterface.php
@@ -6,13 +6,29 @@ use DateTime;
 
 interface SoftDeletableInterface
 {
+    /**
+     * @param null $isDelete
+     * @return boolean
+     */
     public function isDelete($isDelete = null);
 
+    /**
+     * @param DateTime $date
+     */
     public function setDeletedAt(DateTime $date);
 
+    /**
+     * @return DateTime
+     */
     public function getDeletedAt();
 
+    /**
+     * @param $username
+     */
     public function setDeletedBy($username);
 
+    /**
+     * @return string
+     */
     public function getDeletedBy();
 }
--- Toolkit/DoctrineManager/Model/TimestampableInterface.php
+++ Toolkit/DoctrineManager/Model/TimestampableInterface.php
@@ -6,19 +6,43 @@ use DateTime;
 
 interface TimestampableInterface
 {
+    /**
+     * @param DateTime $date
+     */
     public function setCreatedAt(DateTime $date);
 
+    /**
+     * @return DateTime
+     */
     public function getCreatedAt();
 
+    /**
+     * @param DateTime $date
+     */
     public function setUpdatedAt(DateTime $date);
 
+    /**
+     * @return DateTime
+     */
     public function getUpdatedAt();
 
+    /**
+     * @param string $username
+     */
     public function setCreatedBy($username);
 
+    /**
+     * @return string
+     */
     public function getCreatedBy();
 
+    /**
+     * @param string $username
+     */
     public function setUpdatedBy($username);
 
+    /**
+     * @return string
+     */
     public function getUpdatedBy();
 }
