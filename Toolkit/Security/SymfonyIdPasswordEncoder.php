30844b7ba2549b876fb14ce9bd73605774af4fab
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-03-15 10:59:22 +0700

    add password encoder

---
+++ Toolkit/Security/SymfonyIdPasswordEncoder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Symfonian\Indonesia\CoreBundle\Toolkit\Security;
+
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+
+class SymfonyIdPasswordEncoder implements PasswordEncoderInterface
+{
+    public function encodePassword($raw, $salt)
+    {
+        return sha1(sprintf('%s{%s}%s', $salt, sha1('SYMFONYINDONESIA'), $salt));
+    }
+
+    public function isPasswordValid($encoded, $raw, $salt)
+    {
+        return $encoded === $this->encodePassword($raw, $salt);
+    }
+}
